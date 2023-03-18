be97f4e3d7600ad52458c9968135b4ec3d82dc60
Steve Wolf <steven.wolf@penn-interactive.com>
2023-03-18 12:15:01 -0400

    progress on process

--- AdjustReasearchId.pl
+++ AdjustReasearchId.pl
@@ -1,19 +1,23 @@
-# input:	research-filename start-id end-id adjustment
+# input:	research-filename start-id end-id offset
 # output:	stdout
-# example:  ResearchProjectDefinitions.xml 1789 1903 -789
+# example:  perl temp\ResearchProjectDefinitions.xml 1789 1903 -789 XL\ResearchProjectDefinitions.xml
 #   techs from 1789..1903 are adjusted downwards by 789 (1789 -> 1000)
-#   perl .\AdjustReasearchId.pl XL/ResearchProjectDefinitions.xml 1789 1903 -789 > "temp\ResearchProjectDefinitions.xml.txt"
 
 sub ProcessFile
 {
-	my($sourcefile) = $_[0];
+	my($source) = $_[0];
 	my($starting) = $_[1] + 0;
 	my($ending) = $_[2] + 0;
-	my($adjustment) = $_[3] + 0;
+	my($offset) = $_[3] + 0;
+	my($target) = $_[4];
 
-	# print "<!-- AdjustResearchId: starting=$starting, ending=$ending, adjustment=$adjustment -->\n";
+	print "source = $source\n";
+	print "target = $target\n";
+	print "IDs = $starting..$ending\n";
+	print "adj = $offset\n";
 
-	open(SF, $sourcefile) or die "$!";
+	open(SF, '<', $source) or die "$!";
+	open(OF, '>', $target) or die "$!";
 
 	# read in each line of the file
 	while ($line = <SF>)
@@ -28,15 +32,16 @@ sub ProcessFile
 			if ($ResearchProjectId >= $starting && $ResearchProjectId <= $ending)
 			{
 				# adjust the ResearchProjectId
-				$line = sprintf("$1<ResearchProjectId>%d</ResearchProjectId>$3\n", $ResearchProjectId + $adjustment);
+				$line = sprintf("$1<ResearchProjectId>%d</ResearchProjectId>$3\n", $ResearchProjectId + $offset);
 			}
 		}
 
 		# copy this line to output
-		print $line;
+		print OF $line;
 	}
 
 	close(SF);
+	close(OF);
 }
 
-ProcessFile($ARGV[0], $ARGV[1], $ARGV[2], $ARGV[3]);
+ProcessFile($ARGV[0], $ARGV[1], $ARGV[2], $ARGV[3], $ARGV[4]);
--- AdjustRow.pl
+++ AdjustRow.pl
@@ -1,4 +1,4 @@
-# input:	research-filename start-row end-row adjustment
+# input:	research-filename start-row end-row offset
 # output:	stdout
 # example:  ResearchProjectDefinitions.xml 10 999 2
 #   all techs, starting from row 10, are adjusted downwards by 2 rows (e.g. row 10 -> row 12, 11 -> 13, etc...)
@@ -6,14 +6,19 @@
 
 sub ProcessFile
 {
-	my($sourcefile) = $_[0];
+	my($source) = $_[0];
 	my($starting) = $_[1] + 0;
 	my($ending) = $_[2] + 0;
-	my($adjustment) = $_[3] + 0;
+	my($offset) = $_[3] + 0;
+	my($target) = $_[4];
 
-	# print "<!-- AdjustRow: starting=$starting, ending=$ending, adjustment=$adjustment -->\n";
+	print "source = $source\n";
+	print "target = $target\n";
+	print "rows = $starting..$ending\n";
+	print "adjs = $offset\n";
 
-	open(SF, $sourcefile) or die "$!";
+	open(SF, '<', $source) or die "$!";
+	open(OF, '<', $source) or die "$!";
 
 	# read in each line of the file
 	while ($line = <SF>)
@@ -27,15 +32,16 @@ sub ProcessFile
 			if ($row >= $starting && $row <= $ending)
 			{
 				# adjust the row
-				$line = sprintf("$1<Row>%d</Row>$3\n", $row + $adjustment);
+				$line = sprint OFf("$1<Row>%d</Row>$3\n", $row + $offset);
 			}
 		}
 
 		# copy this line to output
-		print $line;
+		print OF $line;
 	}
 
 	close(SF);
+	close(OF);
 }
 
-ProcessFile($ARGV[0], $ARGV[1], $ARGV[2], $ARGV[3]);
+ProcessFile($ARGV[0], $ARGV[1], $ARGV[2], $ARGV[3], $ARGV[4]);
