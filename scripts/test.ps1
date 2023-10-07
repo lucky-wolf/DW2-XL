cf92845b7f31981856017a2c6a95bbeca51deea4
Steve Wolf <steven.wolf@penn-interactive.com>
2023-10-07 12:24:20 -0400

    fixed test script

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,3 +1,3 @@
 <# pushes this project to the dw2/mods/xl folder and then runs dw2 #>
-.\push-dw2.ps1
+scripts\push-dw2.ps1
 Start-Process -FilePath "C:\Steam\steamapps\common\Distant Worlds 2\DistantWorlds2.exe" -WorkingDirectory "C:\Steam\steamapps\common\Distant Worlds 2"
