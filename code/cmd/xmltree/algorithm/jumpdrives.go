9110946f31cbfb4e409a5c4b9bd2adaed00f6717
Steve Wolf <steven.wolf@penn-interactive.com>
2023-10-29 17:08:53 -0400

    successfully build script & ran it

--- code/cmd/xmltree/algorithm/jumpdrives.go
+++ code/cmd/xmltree/algorithm/jumpdrives.go
@@ -7,7 +7,7 @@ import (
 
 func HyperDrives(folder string) (err error) {
 
-	log.Println("TODO...")
+	log.Println("Updates core hyperdrives to have 7 levels off of a common data table")
 
 	// load all component definition files
 	j, err := loadJobFor(folder, "ComponentDefinitions*")
@@ -43,15 +43,15 @@ func (j *job) applyHyperDrives() (err error) {
 			"HyperDriveBlockingInsulation": []float64{10, 15, 20, 25, 30, 35, 40},
 			"HyperDriveEnergyUsage":        []float64{80, 100, 120, 140, 160, 180, 200},
 			"HyperDriveJumpRange":          []float64{160e6, 180e6, 200e6, 230e6, 260e6, 300e6, 340e6},
-			"HyperDriveDriveSpeed":         []float64{300e3, 450e3, 600e3, 900e3, 1200e3, 1800e3, 2400e3},
+			"HyperDriveSpeed":              []float64{300e3, 450e3, 600e3, 900e3, 1200e3, 1800e3, 2400e3},
 			"HyperDriveJumpInitiationTime": []float64{25, 22.5, 20, 17.5, 15, 12.5, 10},
 			"HyperDriveRechargeTime":       []float64{25, 22.5, 20, 17.5, 15, 12.5, 10},
-			"HyperDriveDriveJumpAccuracy":  []float64{2000, 2000, 2000, 2000, 2000, 2000, 2000},
+			"HyperDriveJumpAccuracy":       []float64{2000, 2000, 2000, 2000, 2000, 2000, 2000},
 		},
 		{
 			"HyperDriveEnergyUsage":        []float64{50, 70, 90, 110, 130, 150, 170},
 			"HyperDriveJumpRange":          []float64{200e6, 240e6, 280e6, 330e6, 380e6, 440e6, 500e6},
-			"HyperDriveDriveSpeed":         []float64{400e3, 600e3, 800e3, 1200e3, 1600e3, 2400e3, 3200e3},
+			"HyperDriveSpeed":              []float64{400e3, 600e3, 800e3, 1200e3, 1600e3, 2400e3, 3200e3},
 			"HyperDriveJumpInitiationTime": []float64{12.5, 11.25, 10, 8.75, 7.5, 6.25, 5},
 			"HyperDriveRechargeTime":       []float64{12.5, 11.25, 10, 8.75, 7.5, 6.25, 5},
 		},
@@ -64,8 +64,8 @@ func (j *job) applyHyperDrives() (err error) {
 		"HyperDriveJumpInitiationTime",
 		"HyperDriveJumpRange",
 		"HyperDriveRechargeTime",
-		"HyperDriveDriveSpeed",
-		"HyperDriveDriveJumpAccuracy",
+		"HyperDriveSpeed",
+		"HyperDriveJumpAccuracy",
 	}
 
 	componentSchedule := map[string]map[string]schedule{
@@ -75,9 +75,9 @@ func (j *job) applyHyperDrives() (err error) {
 			"HyperDriveEnergyUsage":        avg,
 			"HyperDriveJumpInitiationTime": good,
 			"HyperDriveRechargeTime":       good,
-			"HyperDriveDriveSpeed":         avg,
+			"HyperDriveSpeed":              avg,
 			"HyperDriveJumpRange":          avg,
-			"HyperDriveDriveJumpAccuracy":  avg,
+			"HyperDriveJumpAccuracy":       avg,
 		},
 		"Sojourn Drive": {
 			"ComponentIonDefense":          avg,
@@ -85,9 +85,9 @@ func (j *job) applyHyperDrives() (err error) {
 			"HyperDriveEnergyUsage":        avg,
 			"HyperDriveJumpInitiationTime": avg,
 			"HyperDriveRechargeTime":       avg,
-			"HyperDriveDriveSpeed":         avg,
+			"HyperDriveSpeed":              avg,
 			"HyperDriveJumpRange":          good,
-			"HyperDriveDriveJumpAccuracy":  avg,
+			"HyperDriveJumpAccuracy":       avg,
 		},
 		"Hyperstream Drive": {
 			"ComponentIonDefense":          avg,
@@ -95,9 +95,9 @@ func (j *job) applyHyperDrives() (err error) {
 			"HyperDriveEnergyUsage":        avg,
 			"HyperDriveJumpInitiationTime": avg,
 			"HyperDriveRechargeTime":       avg,
-			"HyperDriveDriveSpeed":         good,
+			"HyperDriveSpeed":              good,
 			"HyperDriveJumpRange":          avg,
-			"HyperDriveDriveJumpAccuracy":  avg,
+			"HyperDriveJumpAccuracy":       avg,
 		},
 		"Smart Drive": {
 			"ComponentIonDefense":          avg,
@@ -105,15 +105,15 @@ func (j *job) applyHyperDrives() (err error) {
 			"HyperDriveEnergyUsage":        good,
 			"HyperDriveJumpInitiationTime": avg,
 			"HyperDriveRechargeTime":       avg,
-			"HyperDriveDriveSpeed":         avg,
+			"HyperDriveSpeed":              avg,
 			"HyperDriveJumpRange":          good,
-			"HyperDriveDriveJumpAccuracy":  avg,
+			"HyperDriveJumpAccuracy":       avg,
 		},
 	}
 
 	statistics := &j.xfiles[0].stats
 
-	applyStats := func(name string) error {
+	applyStats := func(name string) (err error) {
 
 		// find this drive definition
 		e, _ := j.find("Name", name)
@@ -121,7 +121,7 @@ func (j *job) applyHyperDrives() (err error) {
 			return fmt.Errorf("%s not found", name)
 		}
 
-		componentSchedule, ok := componentSchedule[name]
+		actualSchedule, ok := componentSchedule[name]
 		if !ok {
 			return fmt.Errorf("component schedule for %s not found", name)
 		}
@@ -130,7 +130,25 @@ func (j *job) applyHyperDrives() (err error) {
 		stats := e.Child("Values").Elements()
 		for i, e := range stats {
 			for _, key := range keys {
-				e.Child(key).SetValue(data[componentSchedule[key]][key][i])
+
+				lookup, ok := actualSchedule[key]
+				if !ok {
+					err = fmt.Errorf("schedule for key %s not found", key)
+					return
+				}
+
+				if int(lookup) >= len(data) {
+					err = fmt.Errorf("invalid schedule %d for %s", lookup, name)
+					return
+				}
+
+				values, ok := data[lookup][key]
+				if !ok {
+					err = fmt.Errorf("values for key %s not found", key)
+					return
+				}
+
+				e.Child(key).SetValue(values[i])
 			}
 			statistics.elements++
 			statistics.changed++
