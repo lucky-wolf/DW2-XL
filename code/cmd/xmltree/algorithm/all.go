7558931faa18d5392fe8aa48159627fc05f53575
Steve Wolf <steven.wolf@penn-interactive.com>
2024-01-04 21:15:52 -0500

    got the "All" target working

--- code/cmd/xmltree/algorithm/all.go
+++ code/cmd/xmltree/algorithm/all.go
@@ -6,26 +6,17 @@ func All(folder string) (err error) {
 
 	log.Println("All components and research values will be updated for all algorithms to-date")
 
-	// load all component and research definition files
-	j, err := LoadJobFor(folder, "ComponentDefinitions*.xml", "ResearchProjectDefinitions*.xml")
+	// do components first
+	err = Components(folder)
 	if err != nil {
 		return
 	}
 
-	// apply this transformation
-	err = j.applyComponents()
+	// now do research costs
+	err = ResearchCosts(folder)
 	if err != nil {
 		return
 	}
-
-	// apply this transformation
-	err = j.applyResearchCosts()
-	if err != nil {
-		return
-	}
-
-	// save them all
-	j.Save()
 
 	return
 }
